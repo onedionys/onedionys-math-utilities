b24584e6ba77d4259165152960fe3c341d2b1083
onedionys <onedionys@gmail.com>
2024-03-07 22:43:38 +0700

    chore: initialize function project

---
+++ src/mathUtils.js
@@ -0,0 +1,9 @@
+const mathUtils = {
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => (b !== 0 ? a / b : 'Error: Division by zero'),
+  };
+  
+  module.exports = mathUtils;
+  
